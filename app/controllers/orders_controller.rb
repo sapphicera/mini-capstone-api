dd9713404ac4c807dc75c3c3fe8b509c4aa196c8
Alexandra <belliraxia@gmail.com>
2022-03-22 15:36:25 -0400

    adds user authorization and updates orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,52 +1,40 @@
 class OrdersController < ApplicationController
 
   def create
-    # if current_user.id
-    #   product = Product.find_by(id: params[:product_id])
-    #   order = Order.new(
-    #     user_id: current_user.id,
-    #     product_id: params[:product_id],
-    #     quantity: params[:quantity],
-    #     subtotal: product.price,
-    #     tax: product.tax,
-    #     total: product.total
-    #   )
-    #   order.save ? message = "it worked!!" : message = "IT FAILED MISERABLY"
-    #   render json: {message: message}
-    # else
-    #   render json: {message: "IT FAILED LOSER"}
-    # end
-
-    product = Product.find_by(id:params[:product_id])
-    calculated_subtotal = params[:quantity].to_i * product.price
-    calculated_tax = calculated_subtotal * 0.09
-    calculated_total = calculated_subtotal + calculated_tax
-
-    @order = Order.new(
-    user_id: current_user.id,
-    product_id: params[:product_id],
-    quantity: params[:quantity],
-    subtotal: calculated_subtotal,
-    tax: calculated_tax,
-    total: calculated_total
-    )
-
-    @order.save ? (message = "it worked!!") : (message = "IT FAILED MISERABLY")
-    render template: "orders/show"
+    if current_user
+      product = Product.find_by(id: params[:product_id])
+      calculated_subtotal = params[:quantity].to_i * product.price
+      calculated_tax = calculated_subtotal * 0.09
+      calculated_total = calculated_subtotal + calculated_tax
+  
+      @order = Order.new(
+      user_id: current_user.id,
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total
+      )
+  
+      @order.save
+      render template: "orders/show"
+    end
   end
 
   def show
-    @order = Order.find_by(id: params[:id])
-    if @order.user_id == current_user.id
-      render template: "orders/show"
-    else
-      render json: {}, status: :unauthorized
+    if current_user
+      @order = Order.find_by(id: params[:id])
+      if @order.user_id == current_user.id
+        render template: "orders/show"
+      else
+        render json: {}, status: :unauthorized
+      end
     end
   end
 
   def index
-    @orders = Order.where(user_id: current_user.id)
-    if @orders != nil
+    if current_user
+      @orders = current_user.orders
       render template: "orders/index"
     else
       render json: {}, status: :unauthorized
