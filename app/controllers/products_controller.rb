08b577a6cfb2ba7b5f0c88ac250b037fb8b5c14e
Alexandra <belliraxia@gmail.com>
2022-04-20 15:30:00 -0400

    tries adding admin auth

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  # before_action :authenticate_admin, only: [:create, :update, :destroy]
+  before_action :authenticate_admin, only: [:create, :update, :destroy]
 
   def index
     # category = Category.find_by(name: params[:category])
